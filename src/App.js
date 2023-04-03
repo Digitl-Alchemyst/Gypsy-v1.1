7cd2d7e9a9c18bbbb35b0f539490cfdf12067037
Digitl-Alchemyst <129696730+Digitl-Alchemyst@users.noreply.github.com>
2023-04-03 13:05:41 -0700

    Styled and ready for api connections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import './normalize.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar">
+              </div>
+              <div className="message">
+                hello gypsy
+              </div>
+            </div>
+          </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar chatgpt">
+   
+              </div>
+              <div className="message">
+                hello i am gypsy
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea className="chat-input-textarea" placeholder="To start a conversation with Gypsy, type a message here..."></textarea>
+        </div>
+
+      </section>
+
     </div>
   );
 }
